fac9bf80f204e981322b9e6ab196f5243fc585ec
Alexander Bagatka <alex.bagatka@gmail.com>
2021-06-04 14:45:46 +0300

    Fix back datetime

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const ASENKA_BACK_DATETIME = new Date('2021-06-30T13:40:00');
+export const ASENKA_BACK_DATETIME = new Date('2021-06-30T08:55:00');
 export const ASENKA_START_DATETIME = new Date('2021-03-26T09:00:00');
